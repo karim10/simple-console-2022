fae0c6673699714e6c3faffd79b2f8d7cb1c5db6
KARIM BEN YEZZA <karim.b.yezza@gmail.com>
2022-08-29 14:02:06 +0200

    push build folder

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, "client/public")))
+  .use(express.static(path.join(__dirname, "client/build")))
   .get("/", (req, res) => res.render("index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
